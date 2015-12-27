7eba739c66c866d3ece98790bdb3ca3a704bb566
aabreu <andy.abreu@gmail.com>
2015-12-27 00:54:32 -0500

    Added a separate file to create the tables used by the profile mechanism

--- domain/src/main/resources/db/migration/V2__create_profile_tables.sql
+++ domain/src/main/resources/db/migration/V2__create_profile_tables.sql
@@ -1,10 +1,3 @@
-create table USER (
-    ID varchar(50) not null,
-    FIRST_NAME varchar(50) not null,
-    LAST_NAME varchar(50) not null,
-    REGISTRATION_DATE date not null
-);
-
 create table ADDRESS (
     USER_ID varchar(50) not null,
     STREET varchar(100),
