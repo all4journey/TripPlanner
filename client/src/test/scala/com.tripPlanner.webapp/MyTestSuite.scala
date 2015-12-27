2f32b96b3ee065ac4611c1244e66803816cd547b
Rob Kernick <rjkernick@gmail.com>
2015-12-27 12:54:23 -0500

    Added upickle

--- client/src/test/scala/com.tripPlanner.webapp/MyTestSuite.scala
+++ client/src/test/scala/com.tripPlanner.webapp/MyTestSuite.scala
@@ -1,8 +1,16 @@
 package com.tripPlanner.webapp
+
+import java.time.LocalDateTime
+
 import utest._
 import utest.ExecutionContext.RunNow
 
+import upickle.default._
+
 import scala.concurrent.Future
+
+case class object1(num:Int, str:Option[String])
+case class ello(id:Int, stuff:Seq[object1])
 
 object MyTestSuite extends TestSuite {
   override def tests = TestSuite {
@@ -11,10 +19,20 @@ object MyTestSuite extends TestSuite {
         assert(true)
       }
     }
+    'upickle{
+      val tst = ello(1234, Seq(object1(12345, None)))
+
+      val result = write(tst)
+
+      val de = read[ello](result)
+
+      assert(tst.id == de.id)
+    }
   }
 
   tests.runAsync().map( results => {
     assert(results.toSeq.head.value.isSuccess)
+    assert(results.toSeq(2).value.isSuccess)
   })
 
 }
