fa06044feef06283c7867b12d4fbeff25948c201
Rob Kernick <rjkernick@gmail.com>
2015-12-31 23:02:01 -0500

    Added query for states

--- domain/src/main/scala/com/tripPlanner/domain/StateDao.scala
+++ domain/src/main/scala/com/tripPlanner/domain/StateDao.scala
@@ -4,23 +4,30 @@ import slick.driver.MySQLDriver.api._
 import com.tripPlanner.shared.domain.State
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
-import com.tripPlanner.domain.Tables.{UsState => states}
+import com.tripPlanner.domain.Tables.{UsState => states, UsStateRow => row}
 
 /**
   * Created by aabreu on 12/29/15.
   */
 trait StateDao {
-  def getStates(): Seq[State]
+  def getStates: Future[Seq[State]]
 }
 
 case class StateDaoImpl(db: Database)(implicit ec: ExecutionContext) extends StateDao {
-  def getStates(): Seq[State] = {
+  def getStates: Future[Seq[State]] = {
+
+    val statesList = TableQuery[states]
 
     // hard coding this list for now... it should come out of the DB
-    val newYork = State("NY", "New York")
-    val georgia = State("GA", "Georgia")
-    val pen = State("PA", "Pensylvannia")
+//    val newYork = State("NY", "New York")
+//    val georgia = State("GA", "Georgia")
+//    val pen = State("PA", "Pensylvannia")
 
-    return Seq(newYork, georgia, pen)
+
+    db.run(statesList.result) map {
+      stateList => for {
+        s <- stateList
+      } yield State(s.id, s.description.getOrElse("NotFound"))
+    }
   }
 }
