ae05a587668a0b8ceb6bca9e3a1758e6a8341765
aabreu <andy.abreu@gmail.com>
2016-01-19 20:30:16 -0500

    Added the dynamic text fields for handling multiple addresses and multiple vehicles on the profile forms

--- client/src/main/scala/com/tripPlanner/webapp/pages/PersonalInfoFormJsImpl.scala
+++ client/src/main/scala/com/tripPlanner/webapp/pages/PersonalInfoFormJsImpl.scala
@@ -2,9 +2,11 @@ package com.tripPlanner.webapp.pages
 
 import com.tripPlanner.shared.domain.{PersonalFormData, State, Address}
 import org.scalajs.dom
+import org.scalajs.dom.raw.HTMLElement
 import prickle.Unpickle
-
+import org.scalajs.jquery.{jQuery => $, JQueryXHR, JQueryAjaxSettings}
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSExport
 import scala.util.Success
 import scalatags.JsDom.all._
 
@@ -13,29 +15,60 @@ import scalatags.JsDom.all._
   */
 // $COVERAGE-OFF$
 object PersonalInfoFormJsImpl extends PersonalInfoFormJs {
+  var formIndex:Int = 1
+  var formData: PersonalFormData = null
+
   def run(params: PersonalFormData): Unit = {}
 
   def runWithParams(params: Any): Unit = {
 
-    val formData = Unpickle[ParamType].fromString(js.JSON.stringify(params.asInstanceOf[js.Any])) match {
+    formData = Unpickle[ParamType].fromString(js.JSON.stringify(params.asInstanceOf[js.Any])) match {
       case Success(fd: PersonalFormData) => fd
       case _ => PersonalFormData(None, Seq[Address](), Seq[State]())
     }
 
     val content = dom.document.getElementById("content")
+    content.appendChild(personalInfoForm.render)
 
     if (formData.addresses.isEmpty) {
-      content.appendChild(personalInfoForm(Address("", "", None, State("NONE", "Choose a state"), ""), 1).render)
+      addAddressPanelWithAddButton(None)
     }
     else {
-      var formIndex = 1
-      for (addressItem <- formData.addresses) {
-        content.appendChild(personalInfoForm(addressItem, formIndex).render)
-        formIndex += 1
+      val numberOfAddresses = formData.addresses.size - 1
+      for (x <- 0 until numberOfAddresses) {
+        addAddressPanelWithMinusButton(formData.addresses(x))
       }
+
+      addAddressPanelWithAddButton(Some(formData.addresses.last))
     }
 
-    val stateDropdown = dom.document.getElementById("userState")
+    $(".btn-add").click(addMoreAddressFields _)
+  }
+
+  @JSExport
+  def addAddressPanelWithAddButton(address: Option[Address]): Unit = {
+    val mainFormGroup = dom.document.getElementById("main-form-group")
+    mainFormGroup.appendChild(addressTextFields("btn btn-success btn-add", "glyphicon glyphicon-plus", "has-success", 0).render)
+
+    buildStatesDropDown(0)
+
+    //formIndex += 1
+  }
+
+  @JSExport
+  def addAddressPanelWithMinusButton(address: Address): Unit = {
+    val mainFormGroup = dom.document.getElementById("main-form-group")
+    mainFormGroup.appendChild(addressTextFields("btn btn-danger btn-sub", "glyphicon glyphicon-minus", "has-error", formIndex).render)
+
+    buildStatesDropDown(formIndex)
+
+    formIndex += 1
+  }
+
+  @JSExport
+  def buildStatesDropDown(dropDownIndex: Int): Unit = {
+
+    val stateDropdown = dom.document.getElementById("userState" + dropDownIndex)
 
     for (stateItem <- formData.states) {
       val option = dom.document.createElement("option")
@@ -43,10 +76,63 @@ object PersonalInfoFormJsImpl extends PersonalInfoFormJs {
       option.setAttribute("value", stateItem.id)
       stateDropdown.appendChild(option)
     }
-
   }
 
-  def personalInfoForm(address: Address, formIndex: Int) = div(cls := "container")(
+  @JSExport
+  def addMoreAddressFields(): Unit = {
+    $("#streetAddressDiv0").before(addressTextFields("btn btn-danger btn-sub", "glyphicon glyphicon-minus", "has-error", formIndex).render)
+    $("#plusMinusButton" + formIndex).click({ (hTMLElement: HTMLElement) =>
+      val index = $(hTMLElement).attr("index")
+      $("#addressFieldsDiv" + index).remove()
+    } : js.ThisFunction)
+
+    buildStatesDropDown(formIndex)
+
+    formIndex += 1
+  }
+
+  @JSExport
+  def addressTextFields(buttonClass: String, glyphClass: String,  highlightClass:String, formIndex: Int) = div(id := "addressFieldsDiv" + formIndex)(
+    div(id := "streetAddressDiv" + formIndex, cls := "form-group")(
+    label(cls := "col-lg-3 control-label")("Street Address:"),
+    div(cls := "col-lg-8")(
+      div(cls := "input-group")(
+        input(id := "streetAddress" + formIndex, name := "streetAddress" + formIndex, cls := "form-control", `type` := "text"),
+        span(cls := "input-group-btn")(
+          button(id := "plusMinusButton" + formIndex, cls := buttonClass, `type` := "button", onmouseover := { () =>
+            $("#streetAddressDiv" + formIndex).addClass(highlightClass)
+            $("#stateDiv" + formIndex).addClass(highlightClass)
+            $("#zipCodeDiv" + formIndex).addClass(highlightClass)
+          }, onmouseout := { () =>
+            $("#streetAddressDiv" + formIndex).removeClass(highlightClass)
+            $("#stateDiv" + formIndex).removeClass(highlightClass)
+            $("#zipCodeDiv" + formIndex).removeClass(highlightClass)
+          }, "index".attr := formIndex)(
+          span(cls := glyphClass, style := "font-size:1.45em;"))
+        )
+      )
+    )
+  ),
+  div(id := "stateDiv"+ formIndex, cls := "form-group")(
+    label(cls := "col-lg-3 control-label")("State:"),
+    div(cls := "col-lg-8")(
+      div(cls := "ui-select")(
+        select(id := "userState" + formIndex, name := "userState" + formIndex, cls := "form-control")(
+          option(value := "NONE", selected := "selected")("Choose a state")
+        )
+      )
+    )
+  ),
+  div(id := "zipCodeDiv" + formIndex, cls := "form-group")(
+    label(cls := "col-lg-3 control-label")("Zip code:"),
+    div(cls := "col-lg-8")(
+      input(id := "zipCode" + formIndex, name := "zipCode" + formIndex, cls := "form-control", `type` := "text")
+    )
+  )
+  )
+
+  @JSExport
+  def personalInfoForm = div(cls := "container")(
     div(cls := "row-fluid")(
       div(cls := "col-sm-12 col-sm-offset-4")(
         ul(cls := "nav nav-pills")(
@@ -66,7 +152,7 @@ object PersonalInfoFormJsImpl extends PersonalInfoFormJs {
     div(cls := "row")(
       div(cls := "col-md-10 col-md-offset-1 personal-info")(
         form(cls := "form-horizontal", role := "form")(
-          div(
+          div(id := "main-form-group")(
             div(cls := "form-group")(
               label(cls := "col-lg-3 control-label")("First name:"),
               div(cls := "col-lg-8")(
@@ -78,29 +164,9 @@ object PersonalInfoFormJsImpl extends PersonalInfoFormJs {
               div(cls := "col-lg-8")(
                 input(id := "lastName", name := "lastName", cls := "form-control", `type` := "text")
               )
-            ),
-            div(cls := "form-group")(
-              label(cls := "col-lg-3 control-label")("Street Address:"),
-              div(cls := "col-lg-8")(
-                input(id := "streetAddress", name := "streetAddress", cls := "form-control", `type` := "text")
-              )
-            ),
-            div(cls := "form-group")(
-              label(cls := "col-lg-3 control-label")("State:"),
-              div(cls := "col-lg-8")(
-                div(cls := "ui-select")(
-                  select(id := "userState", name := "userState", cls := "form-control")(
-                    option(value := "NONE", selected := "selected")("Choose a state")
-                  )
-                )
-              )
-            ),
-            div(cls := "form-group")(
-              label(cls := "col-lg-3 control-label")("Zip code:"),
-              div(cls := "col-lg-8")(
-                input(id := "zipCode", name := "zipCode", cls := "form-control", `type` := "text")
-              )
-            ),
+            )
+          ),
+          div(
             div(cls := "form-group")(
               label(cls := "col-lg-5 control-label")(),
               div(cls := "col-md-7")(
@@ -109,8 +175,8 @@ object PersonalInfoFormJsImpl extends PersonalInfoFormJs {
                 input(id := "cancelButton", `type` := "reset", cls := "btn btn-default", value := "Cancel")
               )
             )
-
           )
+
         )
       )
     )
--- client/src/main/scala/com/tripPlanner/webapp/pages/VehicleInfoFormJsImpl.scala
+++ client/src/main/scala/com/tripPlanner/webapp/pages/VehicleInfoFormJsImpl.scala
@@ -2,9 +2,12 @@ package com.tripPlanner.webapp.pages
 
 import com.tripPlanner.shared.domain.Vehicle
 import org.scalajs.dom
+import org.scalajs.dom.raw.HTMLElement
+import org.scalajs.jquery.{jQuery => $, _}
 import prickle.Unpickle
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSExport
 import scala.util.Success
 import scalatags.JsDom.all._
 
@@ -13,27 +16,52 @@ import scalatags.JsDom.all._
   */
 // $COVERAGE-OFF$
 object VehicleInfoFormJsImpl extends VehicleInfoFormJs {
+
+  var formIndex = 1
+  var vehicles: Seq[Vehicle] = null
+
   def run(params: Seq[Vehicle]): Unit = {}
 
   def runWithParams(params: Any): Unit = {
 
-    val vehicles = Unpickle[ParamType].fromString(js.JSON.stringify(params.asInstanceOf[js.Any])) match {
+    vehicles = Unpickle[ParamType].fromString(js.JSON.stringify(params.asInstanceOf[js.Any])) match {
       case Success(vs: Seq[Vehicle]) => vs
       case _ => Seq[Vehicle]()
     }
 
     val content = dom.document.getElementById("content")
+    content.appendChild(vehicleForm.render)
 
     if (vehicles.isEmpty) {
-      content.appendChild(vehicleForm(Vehicle("", "", None, None, None), 1).render)
+      addVehiclePanelWithAddButton(None)
     } else {
-      var formIndex = 1
-      for (vehicleItem <- vehicles) {
-        content.appendChild(vehicleForm(vehicleItem, formIndex).render)
-        formIndex += 1
+      val numberOfVehicles = vehicles.size - 1
+      for (x <- 0 until numberOfVehicles) {
+        addVehiclePanelWithMinusButton(vehicles(x))
       }
+
+      addVehiclePanelWithAddButton(Some(vehicles.last))
     }
-    val vehicleYearDropdown = dom.document.getElementById("userVehicleYear")
+
+    $(".btn-add").click(addMoreVehicleFields _)
+  }
+
+  @JSExport
+  def addMoreVehicleFields: Unit = {
+    $("#yearDiv0").before(vehicleTextFields("btn btn-danger btn-sub", "glyphicon glyphicon-minus", "has-error", formIndex).render)
+    $("#plusMinusButton" + formIndex).click({ (hTMLElement: HTMLElement) =>
+      val index = $(hTMLElement).attr("index")
+      $("#vehicleFieldsDiv" + index).remove()
+    } : js.ThisFunction)
+
+    buildVehicleYearDropDown(formIndex)
+
+    formIndex += 1
+  }
+
+  @JSExport
+  def buildVehicleYearDropDown(dropDownIndex: Int): Unit = {
+    val vehicleYearDropdown = dom.document.getElementById("userVehicleYear" + dropDownIndex)
     for(yearInLoop <- 2016 to 1950 by -1) {
       val option = dom.document.createElement("option")
       option.textContent = yearInLoop + ""
@@ -42,13 +70,80 @@ object VehicleInfoFormJsImpl extends VehicleInfoFormJs {
     }
   }
 
-  def vehicleForm(vehicle: Vehicle, formIndex: Int) = div(cls := "container")(
+  @JSExport
+  def addVehiclePanelWithMinusButton(vehicle: Vehicle): Unit = {
+    val mainFormGroup = dom.document.getElementById("main-form-group")
+    mainFormGroup.appendChild(vehicleTextFields("btn btn-danger btn-sub", "glyphicon glyphicon-minus", "has-error", formIndex).render)
+
+    buildVehicleYearDropDown(formIndex)
+
+    formIndex += 1
+  }
+
+  @JSExport
+  def addVehiclePanelWithAddButton(vehicle: Option[Vehicle]): Unit = {
+    val mainFormGroup = dom.document.getElementById("main-form-group")
+    mainFormGroup.appendChild(vehicleTextFields("btn btn-success btn-add", "glyphicon glyphicon-plus", "has-success", 0).render)
+
+    buildVehicleYearDropDown(0)
+
+    //formIndex += 1
+  }
+
+  @JSExport
+  def vehicleTextFields(buttonClass: String, glyphClass: String,  highlightClass:String, formIndex: Int) = div(id := "vehicleFieldsDiv" + formIndex)(
+    div(id := "yearDiv" + formIndex, cls := "form-group")(
+    label(cls := "col-lg-3 control-label")("Year:"),
+    div(cls := "col-lg-8")(
+      div(cls := "input-group")(
+      div(cls := "ui-select")(
+        select(id := "userVehicleYear" + formIndex, name := "userVehicleYear" + formIndex, cls := "form-control")(
+          option(value := "NONE", selected := "selected")("Choose your vehicle year")
+        )
+      ),
+        span(cls := "input-group-btn")(
+          button(id := "plusMinusButton" + formIndex, cls := buttonClass, `type` := "button", onmouseover := { () =>
+            $("#yearDiv" + formIndex).addClass(highlightClass)
+            $("#makeDiv" + formIndex).addClass(highlightClass)
+            $("#modelDiv" + formIndex).addClass(highlightClass)
+          }, onmouseout := { () =>
+            $("#yearDiv" + formIndex).removeClass(highlightClass)
+            $("#makeDiv" + formIndex).removeClass(highlightClass)
+            $("#modelDiv" + formIndex).removeClass(highlightClass)
+          }, "index".attr := formIndex)(
+            span(cls := glyphClass, style := "font-size:1.45em;"))
+        )
+      )
+    )
+  ),
+    div(id := "makeDiv" + formIndex, cls := "form-group")(
+      label(cls := "col-lg-3 control-label")("Make:"),
+      div(cls := "col-lg-8")(
+        input(id := "make" + formIndex, name := "make" + formIndex, cls := "form-control", `type` := "text")
+      )
+    ),
+    div(id := "modelDiv" + formIndex, cls := "form-group")(
+      label(cls := "col-lg-3 control-label")("Model:"),
+      div(cls := "col-lg-8")(
+        input(id := "model" + formIndex, name := "model" + formIndex, cls := "form-control", `type` := "text")
+      )
+    )
+  )
+
+  @JSExport
+  def vehicleForm(someString: String) = div(cls := "container")(
     div(cls := "row-fluid")(
       div(cls := "col-sm-12 col-sm-offset-4")(
         ul(cls := "nav nav-pills")(
-          li(id := "personalInfoLink", role := "presentation")(
+        if (someString.equals("personalInfoLink")) {
+          li(id := "personalInfoLink", role := "presentation", cls := "active")(
             a(href := "/multiformProfile/personal")("Personal Info")
           ),
+        }
+          else {
+          li(id := "personalInfoLink", role := "presentation")(
+          a(href := "/multiformProfile/personal")("Personal Info"),
+        }
           li(id := "vehicleInfoLink", role := "presentation", cls := "active")(
             a(href := "/multiformProfile/vehicle")("Vehicle Info")
           ),
@@ -62,28 +157,8 @@ object VehicleInfoFormJsImpl extends VehicleInfoFormJs {
     div(cls := "row")(
       div(cls := "col-md-10 col-md-offset-1 personal-info")(
         form(cls := "form-horizontal", role := "form")(
-          div(div(cls := "form-group")(
-            label(cls := "col-lg-3 control-label")("Year:"),
-            div(cls := "col-lg-8")(
-              div(cls := "ui-select")(
-                select(id := "userVehicleYear", name := "userVehicleYear", cls := "form-control")(
-                  option(value := "NONE", selected := "selected")("Choose your vehicle year")
-                )
-              )
-            )
-          ),
-            div(cls := "form-group")(
-              label(cls := "col-lg-3 control-label")("Make:"),
-              div(cls := "col-lg-8")(
-                input(id := "make", name := "make", cls := "form-control", `type` := "text")
-              )
-            ),
-            div(cls := "form-group")(
-              label(cls := "col-lg-3 control-label")("Model:"),
-              div(cls := "col-lg-8")(
-                input(id := "model", name := "model", cls := "form-control", `type` := "text")
-              )
-            )
+          div(id := "main-form-group")(
+
           ),
           div(cls := "form-group")(
             label(cls := "col-lg-5 control-label")(),
@@ -94,6 +169,7 @@ object VehicleInfoFormJsImpl extends VehicleInfoFormJs {
             )
           )
         )
+
       )
     )
 
