f5e402f1b621b6d526e2bcf94184a07d3398c984
aabreu <andy.abreu@gmail.com>
2016-04-02 16:46:45 -0400

    needed a[Failure] for the accord unit tests

--- shared/src/test/scala/com/all4journey/shared/domain/ValidationSpec.scala
+++ shared/src/test/scala/com/all4journey/shared/domain/ValidationSpec.scala
@@ -8,6 +8,6 @@ class ValidationSpec extends SharedTestSpec {
   val invalidAddressWithStreet = Address("0", "0", Some("123 street"), State("NONE", ""), "", "HOME", "Home")
 
   "Address.validatorWithStreet" should "fail with street address populated but state and zip are empty" in {
-      validate(invalidAddressWithStreet)(Address.validatorWithStreet) shouldBe Failure
+      validate(invalidAddressWithStreet)(Address.validatorWithStreet) shouldBe a[Failure]
   }
 }
